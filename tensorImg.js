48b4f1bc7cf6b021f2c59d89f764dc18fde310d0
Sirlam <mdelosab@gmail.com>
2022-05-04 16:43:36 -0300

    evaluativo

--- tensorImg.js
+++ tensorImg.js
@@ -1,23 +1,39 @@
 const img = document.getElementById('img');
 const canvas = document.getElementById('canva');
 const select = document.getElementById('options');
-const forma = document.getElementById('forma');
-const girar = document.getElementById('girar');
-const voltear = document.getElementById('voltear');
 
-console.log(select.value)
+console.log(forma.value)
 
-addEventListener
 
 img.onload = () => {
     let tensor = tf.browser.fromPixels(img);
     console.log(tensor.shape);
     console.log('Memoria usada: ', tf.memory(tensor).numBytes);
 
-    tf.browser.toPixels(tensor.reverse(0), canvas).then(
-        tensor.dispose()
-    );
+    let minW = img.width/2;
+    let minH = img.height/2;
+    let maxW = img.width*2;
+    let maxH = img.height*2;
 
-    console.log('Memoria usada: ', tf.memory(tensor).numBytes);
+    select.addEventListener('input', (e)=>{
+        if(select.value == 'forma'){
+            tf.browser.toPixels(tensor.reverse(2), canvas)
+            console.log('Memoria usada: ', tf.memory(tensor).numBytes);
+        } else if(select.value == 'girar'){
+            tf.browser.toPixels(tensor.reverse(0), canvas)
+            console.log('Memoria usada: ', tf.memory(tensor).numBytes);
+        } else if(select.value == 'voltear'){
+            tf.browser.toPixels(tensor.reverse(1), canvas)
+            console.log('Memoria usada: ', tf.memory(tensor).numBytes);
+        } else if(select.value == 'reducir'){
+            tf.browser.toPixels(tf.tensor.resizeBilinear(tensor, [minH, minW]), canvas)
+            console.log('Memoria usada: ', tf.memory(tensor).numBytes);
+        } else if(select.value == 'agrandar'){
+            tf.browser.toPixels(tf.tensor.resizeBilinear(tensor, [minH, minW]), canvas)
+            console.log('Memoria usada: ', tf.memory(tensor).numBytes);
+        }
+    })
+    
+    
 
 }
